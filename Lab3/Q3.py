b8b1b0e4a6f3134cd11152bf058749502a0b50fe
Jeffin joffy <114131523+Apetun@users.noreply.github.com>
2024-01-27 16:20:54 +0530

    Update Q3.py

---
+++ Lab3/Q3.py
@@ -0,0 +1,84 @@
+class Maze():
+    def __init__(self):
+        self.graph={}
+        self.nodes=[]
+    def display(self):
+        print("Adjacency List:")
+        print(self.graph)
+        
+    def connection(self,origin,dest):
+        if origin in self.graph:
+            self.graph[origin].append(dest)
+        else:
+            self.graph[origin] = [dest]
+
+        if dest not in self.graph:
+            self.graph[dest]=[]
+            
+        self.nodes = sorted(list(self.graph.keys()))
+
+    
+
+    def dfs(self,node,visited, stack,ends):
+    	
+    	if node in ends:
+            stack.append(node)
+            print(stack)
+            return True
+        elif self.graph[node].is_empty():
+            return False
+    		
+    	if node not in visited:
+            visited.add(node)
+                        
+
+            for neighbour in self.graph[node]:
+                if self.dfs(neighbour,visited,stack,ends):
+                    stack.append(node)
+                    return True
+
+
+
+        
+
+
+    
+    def solve(self,start,ends):
+        visited = set() 
+        stack = []
+        self.dfs(start,visited,stack,ends)
+                    
+
+
+
+
+            
+
+def main():
+    g1=Maze()
+    g1.connection(20,15)
+    g1.connection(15,10)
+    g1.connection(10,9)
+    g1.connection(10,5)
+    g1.connection(5,4)
+    g1.connection(9,14)
+    g1.connection(14,13)
+    g1.connection(14,19)
+    g1.connection(19,18)
+    g1.connection(18,17)
+    g1.connection(17,16)
+    g1.connection(17,12)
+    g1.connection(12,11)
+    g1.connection(11,6)
+    g1.connection(6,1)
+    g1.connection(1,2)
+    g1.connection(2,3)
+    g1.connection(3,8)
+    g1.connection(8,7)
+    
+    
+    g1.solve(20,[2,5])
+
+
+if __name__ == "__main__":
+    main()
